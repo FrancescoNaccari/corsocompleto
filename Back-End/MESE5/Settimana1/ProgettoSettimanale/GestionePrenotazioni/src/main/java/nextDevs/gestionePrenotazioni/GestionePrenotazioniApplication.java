1eef9b608a317f00d62ecb473cfe52eab6e74f16
FrancescoNaccari <francesco.n92@gmail.com>
2024-05-17 16:48:44 +0200

    progetto settimanale

--- Back-End/MESE5/Settimana1/ProgettoSettimanale/GestionePrenotazioni/src/main/java/nextDevs/gestionePrenotazioni/GestionePrenotazioniApplication.java
+++ Back-End/MESE5/Settimana1/ProgettoSettimanale/GestionePrenotazioni/src/main/java/nextDevs/gestionePrenotazioni/GestionePrenotazioniApplication.java
@@ -1,4 +1,4 @@
-package nextDevs.GestionePrenotazioni;
+package nextDevs.gestionePrenotazioni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Back-End/MESE5/Settimana1/ProgettoSettimanale/GestionePrenotazioni/src/test/java/nextDevs/gestionePrenotazioni/GestionePrenotazioniApplicationTests.java
+++ Back-End/MESE5/Settimana1/ProgettoSettimanale/GestionePrenotazioni/src/test/java/nextDevs/gestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -1,4 +1,4 @@
-package nextDevs.GestionePrenotazioni;
+package nextDevs.gestionePrenotazioni;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
